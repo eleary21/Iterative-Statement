f1a253279e6f4fa25a2d3112443500295124ced8
Ethan Leary <eleary@usf.edu>
2021-01-29 23:57:16 -0500

    Commit

--- Iterative Statement/Program.cs
+++ Iterative Statement/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//Author: Ethan Leary
+//Date: 1/29/2021
+//Description: Write a iterative statement that displays the number of times a user enters an integer value.
+using System;
 
 namespace Iterative_Statement
 {
@@ -6,7 +9,29 @@ namespace Iterative_Statement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter an integer between 1-100");
+            
+            string inputstr = Console.ReadLine();
+            int input = int.Parse(inputstr);
+            int number;
+            if (input <= 1)
+            {
+                Console.WriteLine("Please pick an integer between 1-100.");
+            }
+            else if (input >= 100)
+
+            {
+                Console.WriteLine("Please pick an integer between 1-100.");
+            }
+            else
+            {
+                for (number = 1; number <= input; number++)
+                {
+
+                    Console.WriteLine("You have entered " + input + ". This is the current integer value in the loop " + number);
+                }
+            }
+            Console.ReadKey(true);
         }
     }
 }
